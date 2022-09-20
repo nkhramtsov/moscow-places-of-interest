668265eb65f7da75816ba5d2cf7a091e06174bbf
nkhramtsov <nkramtsov@gmail.com>
2022-09-20 16:22:12 +0500

    Add frontend

--- where_to_go/views.py
+++ where_to_go/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def show_main_page(request):
-    return HttpResponse('Здесь будет карта')
+def index(request):
+    return render(request, 'index.html')
